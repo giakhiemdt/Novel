b099c772a17ba52bec75db5c7211819da8c37eaa
giakhiemdt <khiem1371@gmail.com>
2026-02-05 08:58:18 +0700

    chore: move back icon

--- FE/src/components/layout/TopBar.tsx
+++ FE/src/components/layout/TopBar.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "../common/Button";
 import { useI18n } from "../../i18n/I18nProvider";
 import { commandRegistry, normalizeCommand } from "../../features/command/commandRegistry";
+import backIcon from "../../assets/icons8-back-90.png";
 
 export type TopBarProps = {
   sidebarOpen: boolean;
@@ -39,7 +40,7 @@ export const TopBar = ({ sidebarOpen, onToggleSidebar, onBack }: TopBarProps) =>
     <div className="topbar">
       <div className="topbar__left">
         <Button variant="ghost" onClick={onBack} className="topbar__button topbar__button--icon">
-          <img src="/icons8-back.apng" alt={t("Back")} className="topbar__icon" />
+          <img src={backIcon} alt={t("Back")} className="topbar__icon" />
         </Button>
         <Button variant="ghost" onClick={onToggleSidebar} className="topbar__button">
           {sidebarOpen ? t("Hide sidebar") : t("Show sidebar")}
